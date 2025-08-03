f7d7a00a5992a25b8d918ba13ca93da47c61175d
Nahiyan Kamal <noyanayihan@gmail.com>
2025-08-03 14:10:12 +0200

    Revert "Convert Docusaurus config to TypeScript and update documentation structure"
    This reverts commit 833f9cc733c7512e7b4ed80314c501b71a838b64.

---
+++ docusaurus.config.js
@@ -0,0 +1,109 @@
+
+module.exports = {
+  title: "Console Table Printer", // Title for your website.
+  tagline: "A tool to print colorful table on your console",
+  url: "https://console-table.netlify.app", // Your website URL
+  baseUrl: "/", // Base URL for your project */
+
+  favicon: "img/favicon.ico",
+  organizationName: "Console Styles", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+  themeConfig: {
+    announcementBar: {
+      id: "support",
+      content:
+        '⭐️ If you like Console Table Printer, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/console-table-printer/console-table-printer">GitHub</a>! ⭐️',
+    },
+    navbar: {
+      title: "Console Table Printer",
+      logo: {
+        alt: "CTP",
+        src: "img/logo.ico",
+      },
+      items: [
+        {
+          href: "https://github.com/console-table-printer/console-table-printer",
+          label: "GitHub",
+          position: "right",
+        },
+        {
+          href: "https://www.npmjs.com/package/console-table-printer",
+          label: "npmjs",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Learn",
+          items: [
+            {
+              label: "Quick Start",
+              to: "docs",
+            },
+            {
+              label: "Getting Started With CLI",
+              to: "docs/doc-cli-install-quick-start",
+            },
+          ],
+        },
+        {
+          title: "Decorate",
+          items: [
+            {
+              label: "Color",
+              to: "docs/doc-color",
+            },
+            {
+              label: "Border",
+              to: "docs/doc-border-design",
+            },
+            {
+              label: "Alignment",
+              to: "docs/doc-alignment",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/console-table-printer/console-table-printer",
+            },
+            {
+              label: "Npmjs",
+              href: "https://www.npmjs.com/package/console-table-printer",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Nahiyan Kamal under MIT License. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          path: "docs",
+          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarCollapsible: false,
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+          remarkPlugins: [
+            [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
+          ],
+          // this enabled the edit button for documentation
+          editUrl:
+            "https://github.com/console-table-printer/console-table-docu/blob/master/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
---
+++ sidebars.js
@@ -0,0 +1,33 @@
+module.exports = {
+  docs: {
+    "Console Table Printer": [
+      "doc-install-quick-start",
+      "doc-table-instance-creation",
+      "doc-adding-rows",
+      "doc-row-divider",
+      "doc-add-columns",
+      "doc-color",
+      "doc-no-ascii",
+      "doc-sort-filter",
+      "doc-alignment",
+      "doc-enable-disable-col",
+      "doc-computed-function",
+      "doc-title",
+      "doc-column-title",
+      "doc-limit-line-width",
+      "doc-border-design",
+      "doc-emojis-special-chars",
+      "doc-render-console",
+      "doc-typescript",
+    ],
+    "Table Printer CLI": [
+      "doc-cli-install-quick-start",
+      "doc-cli-brew"
+    ],
+    "API Reference": [
+      "api/core-functions",
+      "api/table-methods",
+      "api/configuration",
+    ],
+  },
+};
